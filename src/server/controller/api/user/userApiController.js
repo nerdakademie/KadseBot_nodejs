17933b61c0c20598988af657c62154e10a979710
Michel Wohlert <WohlertMi@xnet.oe.olympus>
2016-08-09 10:58:50 +0200

    fix user model object creation

--- src/server/controller/api/user/userApiController.js
+++ src/server/controller/api/user/userApiController.js
@@ -5,7 +5,7 @@ const userHelper = require('../../../helper/userHelper');
 module.exports = (() => {
 
   function register(request, response) {
-    const user = new User(request.body);
+    const user = new User({nak_user: request.body.nak_user, nak_pass: request.body.nak_pass});
     //TODO check if user is actually a nak user
     userHelper.registerUser(user,function(message){
       response.json({status: message});
