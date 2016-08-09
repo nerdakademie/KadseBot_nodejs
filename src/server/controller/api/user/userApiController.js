335ec0c9a5201705a4363688382536d7d5d9e498
Michel Wohlert <WohlertMi@xnet.oe.olympus>
2016-08-09 11:15:13 +0200

    change parameters in userApiController

--- src/server/controller/api/user/userApiController.js
+++ src/server/controller/api/user/userApiController.js
@@ -1,15 +1,20 @@
 const User = require('mongoose').model('User');
 const $ = require('jquery');
 const userHelper = require('../../../helper/userHelper');
+const isJSON = require('is-json');
 
 module.exports = (() => {
 
   function register(request, response) {
-    const user = new User({nak_user: request.body.nak_user, nak_pass: request.body.nak_pass});
-    //TODO check if user is actually a nak user
-    userHelper.registerUser(user,function(message){
-      response.json({status: message});
-    });
+    if(isJSON((request.body)){}
+      const user = new User({nak_user: request.body.username, nak_pass: request.body.password});
+      //TODO check if user is actually a nak user
+      userHelper.registerUser(user,function(message){
+        response.json({status: message});
+      });
+    }else{
+      response.json({status: 'error: expected json'});
+    }
   }
 
   return {
