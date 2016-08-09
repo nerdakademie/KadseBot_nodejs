32ea1c01f5377f0fb5eb2ee995fe4819854b38a4
Michel Wohlert <WohlertMi@xnet.oe.olympus>
2016-08-09 11:25:12 +0200

    Fix userApiController

--- src/server/controller/api/user/userApiController.js
+++ src/server/controller/api/user/userApiController.js
@@ -1,20 +1,15 @@
 const User = require('mongoose').model('User');
 const $ = require('jquery');
 const userHelper = require('../../../helper/userHelper');
-const isJSON = require('is-json');
 
 module.exports = (() => {
 
   function register(request, response) {
-    if(isJSON((request.body)){
-      const user = new User({nak_user: request.body.username, nak_pass: request.body.password});
-      //TODO check if user is actually a nak user
-      userHelper.registerUser(user,function(message){
-        response.json({status: message});
-      });
-    }else{
-      response.json({status: 'error: expected json'});
-    }
+    const user = new User({nak_user: request.body.username, nak_pass: request.body.password});
+    //TODO check if user is actually a nak user
+    userHelper.registerUser(user,function(message){
+      response.json({status: message});
+    });
   }
 
   return {
