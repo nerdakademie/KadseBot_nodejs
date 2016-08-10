dc23f7f9a62acb8f660d203e67dc88c13e625a8c
Michel Wohlert <WohlertMi@xnet.oe.olympus>
2016-08-10 10:33:08 +0200

    userHelper: integrate NAK Auth Cookie userHelper: only allow registration for NAK users

--- src/server/helper/userHelper.js
+++ src/server/helper/userHelper.js
@@ -38,7 +38,7 @@ module.exports = (() => {
 
   function isNAKUser(username,password){
     request.post({url:'https://cis.nordakademie.de/startseite/?no_cache=1', form: {logintype: "login", pid: 0,user: username,pass: password}}, function(err,httpResponse,body){
-      console.log(httpResponse.headers);
+      console.log(httpResponse.headers.set-cookie);
     });
   }
 
