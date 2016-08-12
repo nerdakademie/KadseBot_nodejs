27bee9ed2531932264646e1f38957b2b53c93f5a
Michel Wohlert <michel.wohlert@gmail.com>
2016-08-12 11:27:36 +0200

    test user scraping

--- src/server/helper/utils.js
+++ src/server/helper/utils.js
@@ -5,7 +5,7 @@ module.exports = (() => {
     cheerioHandle('form table tr').each(function (id, elem) {
       const children = cheerioHandle(this).children();
       console.log(children.eq(0));
-      console.log(children.eq(1));
+      console.log(children.eq(1).text());
       /*
        cheerioHandle('td', elem).each(function (id2, elem2) {
         console.log(elem2.text());
