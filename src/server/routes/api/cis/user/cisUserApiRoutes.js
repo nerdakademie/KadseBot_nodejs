1cfc8fcb9e1fa1bd3ee67e3ffa2bc7425d2399c5
Michel Wohlert <michel.wohlert@gmail.com>
2016-08-22 14:15:15 +0200

    rename Seminars to SeminarsParticipated

--- src/server/routes/api/cis/user/cisUserApiRoutes.js
+++ src/server/routes/api/cis/user/cisUserApiRoutes.js
@@ -5,8 +5,8 @@ const router = require('express').Router();
 router.post('/getAuthCookie', cisUserApiController.getNAKAuthCookie);
 router.get('/getUserDetails', cisUserApiController.getUserDetails);
 router.get('/getGrades', cisUserApiController.getGrades);
-router.get('/getSeminarsParticipated', cisUserApiController.getSeminars);
-router.get('/getAvailableSeminars',cisUserApiController.getAvailableSeminars);
+router.get('/getSeminarsParticipated', cisUserApiController.getSeminarsParticipated);
+router.get('/getAvailableSeminars', cisUserApiController.getAvailableSeminars);
 router.use('/auth', cisUserAuthApiRoutes);
 
 module.exports = router;
