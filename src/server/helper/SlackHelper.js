f5aac1224e42676872513d22f53b378a4c9cb77c
Michel Wohlert <michel.wohlert@gmail.com>
2017-03-14 13:35:07 +0000

    changed name and icon of slackbot

--- src/server/helper/SlackHelper.js
+++ src/server/helper/SlackHelper.js
@@ -2,7 +2,9 @@ const SlackBot = require('slackbots');
 const config = require('config');
 const StringHelper = require('./StringHelper');
 const bot = new SlackBot({
-  token: config.get('slack_bot_token')
+  token: config.get('slack_bot_token'),
+  name: 'TelegramBridge',
+  icon_url: 'https://raw.githubusercontent.com/telegramdesktop/tdesktop/master/Telegram/Resources/art/icon128.png'
 });
 
 module.exports = (() => {
